aebfb624e4abdd1cab4f3bc061e6d29d738e80e4
sanjeeb subedi <sanjeebsubedi4@gmail.com>
2021-02-24 19:32:19 +0545

    changed mean and std to imagenet's values

--- variables.py
+++ variables.py
@@ -4,8 +4,8 @@ from efficientnet_pytorch import EfficientNet
 root_dir = '/content/drive/MyDrive/Inspiring/datasets_train'
 model_name = 'efficientnet-b2'
 image_size = EfficientNet.get_image_size(model_name)
-rgb_mean = [0.0176, 0.0169, 0.0160]
-rgb_std = [0.1016, 0.0977, 0.0905]
+rgb_mean = [0.485, 0.456, 0.406]
+rgb_std = [0.229, 0.224, 0.225]
 num_workers = multiprocessing.cpu_count()
 train_batch_size = 128
 valid_batch_size = 64
