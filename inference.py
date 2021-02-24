f1f4ce3538ef2d7da61be58a67a5d53286ca9163
sanjeeb subedi <sanjeebsubedi4@gmail.com>
2021-02-24 19:59:08 +0545

    imagenet mean,std & inference now on more than 1 image inside folder

--- inference.py
+++ inference.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch
 from efficientnet_pytorch import EfficientNet
 import numpy as np
+import os
 
 class InferenceModel(nn.Module):
   def __init__(self, model_name):
@@ -15,8 +16,8 @@ class InferenceModel(nn.Module):
             nn.Linear(in_ftrs, 512),
             nn.BatchNorm1d(512),
             nn.Dropout(0.2),
-            nn.Linear(512, 256),
-            nn.Linear(256 , 9)
+            nn.Linear(512, 9)
+            # nn.Linear(256 , 9)
         )
 
   def forward(self, inputs):
@@ -28,25 +29,24 @@ class InferenceModel(nn.Module):
     return x
 
 
-def index_to_label(idx):
-  indices = {'ACE': 0, 'BET': 1, 'CORSTER': 2, 'CUP': 3, 'FRA': 4, 'MOR': 5, 'PLA': 6, 'POP': 7, 'SAL': 8}
-  indices = {y:x for x,y in indices.items()}
-  temp = indices.get(idx)
+def index_to_label(idx, test_dir):
+  classes = sorted(os.listdir(test_dir))
+  temp = classes[idx]
   return temp
 
 def preprocess_img(img):
   if isinstance(img, str):
     image = Image.open(img).convert('RGB')
-  elif isinstance(img, np.ndarray):
-    image = Image.fromarray(img).convert('RGB')
-  elif isinstance(img, torch.Tensor):
-    image = Image.fromarray(img.numpy()).convert('RGB')
-  else:
-    raise Exception(f'type of `img` should be any of tensor, numpy array or a path to an image, not {type(img)}.')
+  # elif isinstance(img, np.ndarray):
+  #   image = Image.fromarray(img).convert('RGB')
+  # elif isinstance(img, torch.Tensor):
+  #   image = Image.fromarray(img.numpy()).convert('RGB')
+  # else:
+  #   raise Exception(f'type of `img` should be any of tensor, numpy array or a path to an image, not {type(img)}.')
   
-  tfms = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor(),
-                                  transforms.Normalize([0.0176, 0.0169, 0.0160],
-                                                       [0.1016, 0.0977, 0.0905])])
+  tfms = transforms.Compose([transforms.Resize(image_size),transforms.ToTensor(),
+                             transforms.Normalize([0.485, 0.456, 0.406],
+                                                  [0.229, 0.224, 0.225])])
   image = tfms(image)
   image = image.unsqueeze(0)
   return image
@@ -55,16 +55,24 @@ def predict(model, preprocessed_img):
   model.eval()
   pred = model(preprocessed_img)
   pred = np.argmax(pred.detach().numpy())
-  res = index_to_label(pred)
-  return res
+  return pred
 
-model_name = 'efficientnet-b0'
+model_name = 'efficientnet-b2'
+global image_size 
+image_size = EfficientNet.get_image_size(model_name)
 model_path = '/content/drive/MyDrive/Inspiring/models/pollen-eff-b2.pt'
-img_path = '/content/drive/MyDrive/Inspiring/datasets_train/POP/20191121T003200362-147-5.png'
+test_dir = '/content/drive/MyDrive/Inspiring/datasets_test'
 
 loaded_model = InferenceModel(model_name)
 loaded_model.load_state_dict(torch.load(model_path, map_location='cpu'))
 
-pp_img = preprocess_img(img_path)
-res = predict(loaded_model, pp_img)
-print(res)
+res = []
+for root_dir, subdir, files in os.walk(test_dir):
+  for img in files:
+    img_path = os.path.join(root_dir, img)
+    pp_img = preprocess_img(img_path)
+    pred = predict(loaded_model, pp_img)
+    res.append(index_to_label(pred, test_dir))
+from collections import Counter
+counts = Counter(res)
+print(counts)
